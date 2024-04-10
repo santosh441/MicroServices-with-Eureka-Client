63daa71735548423bd7ead153b2e2162b46d4fe0
santosh441 <165950598+santosh441@users.noreply.github.com>
2024-04-10 13:23:28 -0400

    Add files via upload

---
+++ quiz-service/src/main/java/com/project/quizservice/controller/QuizController.java
@@ -0,0 +1,39 @@
+package com.project.quizservice.controller;
+
+
+import com.project.quizservice.model.QuestionWrapper;
+import com.project.quizservice.model.QuizDto;
+import com.project.quizservice.model.Response;
+import com.project.quizservice.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestBody QuizDto quizDto){
+        System.out.println(quizDto.getCategoryName());
+        return quizService.createQuiz(quizDto.getCategoryName(), quizDto.getNumQuestions(), quizDto.getTitle());
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
+}
